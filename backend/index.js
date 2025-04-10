9cf5fa36b1d8ea5c3fa5006f2a18251203f7a7a4
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-04-10 11:24:25 +0200

    Beginning routes

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,26 @@ const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
 const port = 1337;
 
-app.get("/", (req, res) => {
-	res.status(200).send("Hello World");
+app.use(express.json());
+
+app.get("/planets-list", (req, res) => {
+	console.log("Fångar planeternas namn");
+	res.status(200).send("Returning Planets List");
+});
+
+app.get("/planet/:id", (req, res) => {
+	console.log("Fångar specifik planet");
+	res.status(200).send("Returning Planet Info");
+});
+
+app.post("/reset", (req, res) => {
+	console.log("Resettar Planeterna");
+	res.status(200).send("Vi resettar planeterna");
+});
+
+app.post("/planet/:id", (req, res) => {
+	console.log("Uppdaterar specifik planet");
+	res.status(200).send("Redigerar planet");
 });
 
 app.listen(port, () => {
