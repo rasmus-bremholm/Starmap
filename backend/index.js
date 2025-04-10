0002e20d2c2893076ce13b1b6ab2cd9bdf01a480
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-04-10 13:34:34 +0200

    login route and planet data

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,15 @@ const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
 const port = 1337;
 
+const planetData = null;
+const planetDataBackup = JSON.parse(JSON.stringify(planetData));
+
 app.use(express.json());
+
+function mymiddleWare(req, res, next) {
+	console.log("Testar middleware");
+	next();
+}
 
 app.get("/planets-list", (req, res) => {
 	console.log("Fångar planeternas namn");
@@ -21,9 +29,17 @@ app.post("/reset", (req, res) => {
 	res.status(200).send("Vi resettar planeterna");
 });
 
-app.post("/planet/:id", (req, res) => {
+app.post("/planet/:id", mymiddleWare, (req, res) => {
 	console.log("Uppdaterar specifik planet");
 	res.status(200).send("Redigerar planet");
+});
+
+app.post("/login", (req, res) => {
+	/*
+		Användaren behöver ange rätt login för att kunna redigera städer. För att komma till edit sidan så letar React efter en variabel i session storage.
+	*/
+	console.log("Loggar in");
+	res.status(200).send("Loggar in");
 });
 
 app.listen(port, () => {
