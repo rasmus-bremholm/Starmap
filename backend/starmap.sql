d48274b37da8a62ce822aebb9c185e52e01bdfe2
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-04-25 12:05:52 +0200

    Installed Cookie Parser and Login logic

--- backend/starmap.sql
+++ backend/starmap.sql
@@ -1,11 +1,20 @@
-
+DROP TABLE IF EXISTS planet_types;
 DROP TABLE IF EXISTS planets;
 DROP TABLE IF EXISTS systems;
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE planets (id INTEGER PRIMARY KEY AUTOINCREMENT,system INTEGER NOT NULL, name CHAR(20) UNIQUE NOT NULL, description TEXT, image TEXT DEFAULT ('./placeholder.png'), FOREIGN KEY (system) REFERENCES systems(id))
-CREATE TABLE systems (id INTEGER PRIMARY KEY AUTOINCREMENT, name CHAR(20) UNIQUE NOT NULL, description TEXT)
-CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, email CHAR(20) UNIQUE NOT NULL, password TEXT NOT NULL, isAdmin BOOLEAN NOT NULL DEFAULT (false))
 
---Users
-INSERT INTO users (email, password, isAdmin) VALUES (rasmus@iths.se, abc123, true)
+CREATE TABLE planets (id INTEGER PRIMARY KEY AUTOINCREMENT,
+   system INTEGER NOT NULL,
+   name CHAR(20) UNIQUE NOT NULL,
+   description TEXT,
+   image TEXT DEFAULT ('./placeholder.png'),
+   planet_type INTEGER NOT NULL ,
+   FOREIGN KEY (system) REFERENCES systems(id),
+   FOREIGN KEY (planet_type) REFERENCES planet_types(id));
+CREATE TABLE planet_types(id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name CHAR(20) UNIQUE NOT NULL,
+   description TEXT,
+   planets INTEGER, FOREIGN KEY (planets) REFERENCES planets(id));
+CREATE TABLE systems (id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name CHAR(20) UNIQUE NOT NULL,
+   description TEXT);
