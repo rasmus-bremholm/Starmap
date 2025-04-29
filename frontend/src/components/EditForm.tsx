7aafbab741ef46b7bd160fbedb73c70cf841c28e
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-04-29 13:46:57 +0200

    Edit form

--- frontend/src/components/EditForm.tsx
+++ frontend/src/components/EditForm.tsx
@@ -1,3 +1,43 @@
-export default function EditForm() {
-	return <div></div>;
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import type { PlanetData } from "../utils/types";
+
+interface EditActionProps {
+	action: string | undefined;
+}
+
+const EditContainer = styled.main`
+	height: 100%;
+`;
+
+export default function EditForm(action: EditActionProps) {
+	const [editAction, setEditAction] = useState(action.action);
+	const [planetList, setPlanetList] = useState<PlanetData[]>();
+	const [activePlanet, setActivePlanet] = useState("");
+
+	useEffect(() => {
+		fetch("http://localhost:1337/planets-list")
+			.then((response) => response.json())
+			.then((result) => {
+				setPlanetList(result);
+			});
+	}, []);
+
+	return (
+		<main className='planet-info-container'>
+			<EditContainer>
+				{editAction && <h1>{editAction} Planet</h1>}
+				<div>
+					<form>
+						<label htmlFor='select-planet'>Choose Planet:</label>
+						<select name='select-planet' onChange={(event) => setActivePlanet(event.target.value)} value={activePlanet}>
+							{planetList?.map((planet) => (
+								<option>{planet.title}</option>
+							))}
+						</select>
+					</form>
+				</div>
+			</EditContainer>
+		</main>
+	);
 }
--- frontend/src/views/EditView.tsx
+++ frontend/src/views/EditView.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import LoginForm from "../components/LoginForm";
 import EditForm from "../components/EditForm";
+import { useParams } from "react-router-dom";
 
 export default function EditView() {
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isAuthenticated, setIsAuthenticated] = useState(true); // <-- ÄNDRA NÄR VI ÄR KLARA
+	const action = useParams();
 
 	useEffect(() => {
 		// Do nothing
@@ -11,7 +13,7 @@ export default function EditView() {
 
 	return (
 		<>
-			<main>{!isAuthenticated ? <LoginForm onLogin={() => setIsAuthenticated(true)} /> : <EditForm />}</main>
+			<main>{!isAuthenticated ? <LoginForm onLogin={() => setIsAuthenticated(true)} /> : <EditForm action={action.action} />}</main>
 		</>
 	);
 }
